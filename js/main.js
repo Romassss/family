f3431c6cce83727b86101437dc826d9e01acb7c7
Ati <attila.irhazi@gmail.com>
2020-09-20 22:12:21 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+let feliratok = ["Irházi-Család", "Anya", "Apa", "Iza", "Zsombi"];
+
+function getImgTitle(a) {
+    let c = document.getElementById("felirat");
+    let ctx = c.getContext("2d");
+    ctx.font = "41px Comic Sans MS";
+    ctx.fillStyle = "red";
+    ctx.fillText(feliratok[a], 40, 80);
+}
+
+let b;
+
+function delayedFelirat() {
+    getImgTitle(b);
+}
+
+function clearFelirat() {
+    let c = document.getElementById("felirat");
+    let ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+}
+
+$('#csaladGomb').click(function () {
+    $("#change-image").fadeOut(1200, function () {
+        $('#change-image').attr('src', "img/csalad.jpg");
+        $("#change-image").fadeIn(1200);
+    });
+    b = 0;
+    clearFelirat();
+    setTimeout(delayedFelirat, 2500);
+}
+);
+
+$('#anyaGomb').click(function () {
+    $("#change-image").fadeOut(1200, function () {
+        $('#change-image').attr('src', "img/Anya.jpg");
+        $("#change-image").fadeIn(1200);
+    });
+    b = 1;
+    clearFelirat();
+    setTimeout(delayedFelirat, 2500);
+}
+);
+
+$('#apaGomb').click(function () {
+    $("#change-image").fadeOut(1200, function () {
+        $('#change-image').attr('src', "img/Apa.jpg");
+        $("#change-image").fadeIn(1200);
+    });
+    b = 2;
+    clearFelirat();
+    setTimeout(delayedFelirat, 2500);
+}
+);
+
+$('#izaGomb').click(function () {
+    $("#change-image").fadeOut(1200, function () {
+        $('#change-image').attr('src', "img/Iza.jpeg");
+        $("#change-image").fadeIn(1200);
+    });
+    b = 3;
+    clearFelirat();
+    setTimeout(delayedFelirat, 2500);
+}
+);
+
+$('#zsombiGomb').click(function () {
+    $("#change-image").fadeOut(1200, function () {
+        $('#change-image').attr('src', "img/Zsombi.jpg");
+        $("#change-image").fadeIn(1200);
+    });
+    b = 4;
+    clearFelirat();
+    setTimeout(delayedFelirat, 2500);
+}
+);
+
+
